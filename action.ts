72d50160cbe7e92fd431b182c95fbf244b364601
Katayama Yutaro <katayamayutaro@team-lab.com>
2021-07-12 16:50:47 +0900

    first commit

--- action.ts
+++
@@ -1,5 +0,0 @@
-import { Action } from './types';
-
-export const addCountAction: Action = {
-  type: 'ADD_COUNT',
-};
--- types.ts
+++
@@ -1,2 +0,0 @@
-export type State = { title: string; count: number };
-export type Action = { type: string };
